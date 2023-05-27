cc2e73aac048d382540a3dd20c90eb5a5b647534
PaveLL <78543974+P-a-V-e-LL@users.noreply.github.com>
2023-05-27 12:40:38 +0300

    Update resnet50_RandomInputTransformation_imagenet_train.py
    FIx normalize for Imagenet

--- resnet50_RandomInputTransformation_imagenet_train.py
+++ resnet50_RandomInputTransformation_imagenet_train.py
@@ -78,7 +78,7 @@ def main():
     Pad((random.randint(0, 35), random.randint(0, 35), random.randint(0, 35), random.randint(0, 35))),
     Resize((224, 224)),
     transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     #trainset = torchvision.datasets.ImageNet(root=args['data_path'], train=True, download=True, transform=transform)
