98342de2fd1b5d45d80af991d27f02ebabed0475
PaveLL <78543974+P-a-V-e-LL@users.noreply.github.com>
2023-06-02 21:00:47 +0300

    Added best val_loss save

--- resnet50_fgsm_imagenet_train.py
+++ resnet50_fgsm_imagenet_train.py
@@ -92,6 +92,7 @@ def main():
 
     train_loss = []
     val_loss = []
+    val_running_loss = float('inf')
 
     start = time.time()
 
@@ -137,6 +138,9 @@ def main():
 
             val_running_loss += loss.item()
         val_loss.append(val_running_loss)
+        if val_running_loss < best_val_loss:
+            best_val_loss = val_running_loss
+            torch.save(model.state_dict(), './models/resnet50_fgsm_best_val.pth')
         scheduler.step(val_running_loss)
         print(f'Epoch {epoch+1} - train loss {running_loss} - val loss {val_running_loss}')
 
